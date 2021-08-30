23e4850ec2b0a3cef071bf0e5244112769e7a6f0
Centraldosplayers <69520226+Centraldosplayers@users.noreply.github.com>
2021-08-30 10:37:32 -0300

    Add files via upload

---
+++ xatspace_theStore/data.js
@@ -0,0 +1,76 @@
+////////////DADOS PRA ALTERAR A PÁGINA///////////////
+
+
+/////////////////BANNERS//////////////////////////
+
+var banner_one = {
+    img: "https://i.postimg.cc/sj5ycYvt/bg-player.jpg",
+    title: "Faça Parte Você Também",
+    sub_title: "Clube TheStore de Vantagens!",
+    description: "Com o clube TheStore, se divertir é um barato..."
+}
+
+var banner_two = {
+    img: "https://i.postimg.cc/bzjbtP2g/banner.jpg",
+    title: "Faça Parte",
+    sub_title: "Clube TheStore de Vantagens!",
+    description: "Com o clube TheStore, se divertir é um barato..."
+}
+
+///////////////////END BANNERS//////////////////
+
+///////////////////BLOCK YOUR///////////////////////
+
+var block_a = {
+    day: "12",
+    limit: "100"
+}
+
+var block_b = {
+    xats: "10.000",
+    value: "112,50"
+}
+
+var block_c = {
+    value: "160.000"
+}
+
+///////////////////END BLOCK YOUR///////////////////////
+
+
+///////////////////BLOCK RADIO///////////////////////
+
+var block_a_r = {
+    xats: "10.000"
+}
+
+var block_b_r = {
+    value: "15.000",
+    donation: "60"
+}
+
+var block_c_r = {
+    day: "12",
+    limit: "300"
+}
+
+///////////////////END BLOCK RADIO///////////////////////
+
+
+///////////////////ANNOUNCE TWO///////////////////////
+
+var announce = {
+    value: "20",
+    xats: "10.000"
+}
+
+///////////////////END ANNOUNCE///////////////////////
+
+
+///////////////////WHATSAPP///////////////////////
+
+var whatsapp_numer = {
+    number: "+55 31 99232 2822"
+}
+
+///////////////////END WHATSAPP///////////////////////
---
+++ xatspace_theStore/jason.js
@@ -0,0 +1,58 @@
+(function(){
+
+    var btn = $(".jd-btn-top");
+    var back = $(".jd-back-top");
+
+    btn.on('click', function(){
+
+        var select = $(this).attr("href");
+        var position = $(select).offset().top;
+
+        console.log(position);
+        $("body, html").animate({
+            scrollTop: position - 150
+        }, 1000);
+        
+    })
+
+    back.on('click', function(){
+
+        $("body, html").animate({
+            scrollTop: 0
+        }, 1300);
+    })
+
+    /////////////CONTENT PAGE////////////////
+
+    $("#img-banner-one").attr("src", banner_one.img)
+    $("#img-banner-two").attr("src", banner_two.img)
+
+    $("#title-banner").text(banner_one.title)
+    $("#subtitle-banner").text(banner_one.sub_title)
+    $("#description-banner").text(banner_one.description)
+
+    $("#title-banner-two").text(banner_two.title)
+    $("#subtitle-banner-two").text(banner_two.sub_title)
+    $("#description-banner-two").text(banner_two.description)
+
+    $("#your-days-a").text(block_a.day)
+    $("#your-limit-a").text(block_a.limit)
+
+    $("#your-xats-b").text(block_b.xats)
+    $("#your-value-b").text(block_b.value)
+
+    $("#your-xats-c").text(block_c.xats)
+
+    $("#radio-xats-a").text(block_a_r.xats)
+
+    $("#radio-value-b").text(block_b_r.value)
+    $("#donation").text(block_b_r.donation)
+
+    $("#radio-days-c").text(block_c_r.day)
+    $("#radio-limit-c").text(block_c_r.limit)
+
+    $("#announce-xat").text(announce.xats)
+    $("#announce-value").text(announce.value)
+
+    $("#whatsapp-number").text(whatsapp_numer.number)
+})();
