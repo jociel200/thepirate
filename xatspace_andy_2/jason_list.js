a56391ea82065e6c0e204d4ac7def4cf6d42b250
Centraldosplayers <69520226+Centraldosplayers@users.noreply.github.com>
2021-10-07 11:14:39 -0300

    Add files via upload

---
+++ xatspace_andy_2/jason_list.js
@@ -0,0 +1,33 @@
+
+var friends = [
+    {
+        name: 'Tia Maah',
+        user: 'Maah',
+        url: 'sinistra16'
+    },
+    {
+        name: 'Mana Eloh',
+        user: 'Eloh',
+        url: 'ELoh'
+    },
+    {
+        name: 'Tia Paula',
+        user: 'Ana Paula',
+        url: 'anapaula'
+    },
+    {
+        name: 'Mana Anneh',
+        user: 'Anneh',
+        url: 'xAnnezinha'
+    },
+    {
+        name: 'Primo Dimi',
+        user: 'Dimi',
+        url: 'Dimittrius'
+    },
+    {
+        name: 'Mana Misty',
+        user: 'Mistynha',
+        url: 'Mistynha'
+    }
+];
