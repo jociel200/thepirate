75b8674147e7077d45bcf8d59ff5e4cf84842750
Centraldosplayers <69520226+Centraldosplayers@users.noreply.github.com>
2021-10-08 09:26:58 -0300

    Add files via upload

--- xatspace_andy_2/jason_list.js
+++ xatspace_andy_2/jason_list.js
@@ -3,31 +3,37 @@ var friends = [
     {
         name: 'Tia Maah',
         user: 'Maah',
-        url: 'sinistra16'
+        url: 'sinistra16',
+        img: 'https://i.postimg.cc/8cXVh7bm/maah.jpg'
     },
     {
         name: 'Mana Eloh',
         user: 'Eloh',
-        url: 'ELoh'
+        url: 'ELoh',
+        img: 'https://i.postimg.cc/YqDggcSk/eloh.jpg'
     },
     {
         name: 'Tia Paula',
         user: 'Ana Paula',
-        url: 'anapaula'
+        url: 'anapaula',
+        img: 'https://i.postimg.cc/dVzyH8dx/paula.jpg'
     },
     {
         name: 'Mana Anneh',
         user: 'Anneh',
-        url: 'xAnnezinha'
+        url: 'xAnnezinha',
+        img: 'https://i.postimg.cc/y6P0134D/anneh.jpg'
     },
     {
         name: 'Primo Dimi',
         user: 'Dimi',
-        url: 'Dimittrius'
+        url: 'Dimittrius',
+        img: 'https://i.postimg.cc/vHZfc8b3/dimi.jpg'
     },
     {
         name: 'Mana Misty',
         user: 'Mistynha',
-        url: 'Mistynha'
+        url: 'Mistynha',
+        img: 'https://i.postimg.cc/7ZCznBCz/misty.jpg'
     }
 ];
