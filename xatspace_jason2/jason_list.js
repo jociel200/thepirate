2b810d4cc25ee467dced0f611d30612903719a6c
Centraldosplayers <69520226+Centraldosplayers@users.noreply.github.com>
2021-11-03 15:54:19 -0300

    Add files via upload

---
+++ xatspace_jason2/jason_list.js
@@ -0,0 +1,52 @@
+
+var audios = [
+    {
+        cover: 'https://i.postimg.cc/CKKGkKQs/john-legend.jpg',
+        song: 'All Of Me',
+        artist: 'John Legend',
+        path: 'all_of_me.mp3',
+        time: '04:27'
+    },
+    {
+        cover: 'https://i.postimg.cc/26shs780/ed-sheeran.jpg',
+        song: 'Shape Of You',
+        artist: 'Ed Sheeran',
+        path: 'ed_sheeran.mp3',
+        time: '03:55'
+    },
+    {
+        cover: 'https://i.postimg.cc/9QbPD9Jf/ateu.jpg',
+        song: 'Ateo',
+        artist: 'Nathy Peluso',
+        path: 'ateo.mp3',
+        time: '04:27'
+    },
+    {
+        cover: 'https://i.postimg.cc/W4fMnvRL/gemeliers.jpg',
+        song: 'Ya No Me Duele',
+        artist: 'Gemeliers',
+        path: 'gemeliers.mp3',
+        time: '03:37'
+    },
+    {
+        cover: 'https://i.postimg.cc/L4PzzwNw/eminem.jpg',
+        song: 'Love Yourself',
+        artist: 'Eminem',
+        path: 'eminem.mp3',
+        time: '05:23'
+    },
+    {
+        cover: 'https://i.postimg.cc/k46WYp9V/johnny.jpg',
+        song: 'Hurt',
+        artist: 'Johnny Cash',
+        path: 'Johnny_Cash.mp3',
+        time: '03:17'
+    },
+    {
+        cover: 'https://i.postimg.cc/sx57sGMs/ana-vilela.jpg',
+        song: 'Trem Bala',
+        artist: 'Ana Vilela',
+        path: 'ana_vilela.mp3',
+        time: '03:00'
+    }
+]
